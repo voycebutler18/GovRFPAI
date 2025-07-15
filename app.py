270942450d33f4cb07244f86d25f735433c67540
voycebutler18 <voycebutler1@gmail.com>
2025-07-14 21:38:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify, session, redirect, url_for
 from datetime import datetime, timedelta
+from functools import wraps
 import secrets
 import json
 import os
